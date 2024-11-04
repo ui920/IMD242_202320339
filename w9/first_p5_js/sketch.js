31e3fa9356add1d7cf664764bf93ca9f5881657a
UI920 <180168040+ui920@users.noreply.github.com>
2024-11-04 18:58:45 +0900

    2_p5

--- w9/first_p5_js/sketch.js
+++ w9/first_p5_js/sketch.js
@@ -1,8 +1,32 @@
+let tileNumX = 16;
+let tileNumY = 12;
+
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 50);
+  background(0);
+  noStroke();
+  fill('skyblue');
+  //let or var로 변수 설정 int/float와 같은 타입구분x
+  //   for (let column = 0; column < width; column += 40) {
+  //     for (let row = 0; row < width; row += 40) {
+  //       let x = 20 + column;
+  //       let y = 20 + row;
+  //       let diameter = 30;
+  //       circle(x, y, diameter);
+  //     }
+  //   }
+  // }
+
+  for (let row = 0; row < tileNumY; row++) {
+    for (let column = 0; column < tileNumX; column++) {
+      let tileW = width / tileNumX;
+      let tileH = height / tileNumY;
+      let x = tileW * 0.5 + column * tileW;
+      let y = tileH * 0.5 + row * tileH;
+      ellipse(x, y, tileW, tileH);
+    }
+  }
 }
